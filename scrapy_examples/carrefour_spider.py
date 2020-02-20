fa608904845dd2c5f1255ee8d8a155d74eb5b8f7
Mikel <mikel.losada@gmail.com>
2020-02-20 21:09:53 +0100

    Yield url, name, price and url of all Carrefour tables

--- scrapy_examples/carrefour_spider.py
+++ scrapy_examples/carrefour_spider.py
@@ -8,16 +8,43 @@ class CarrefourSpider(scrapy.Spider):
     start_urls = ['https://www.carrefour.es/mesas-mesa-de-estudio-oficina/N-10ftbh0Z1jvbjnb/c']
     download_delay = 0.5
 
+    
     def __init__(self):
         self.headers={'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'}
-    
+        self.site_base_url = 'https://www.carrefour.es'
+        
     def start_requests(self):
         for url in self.start_urls:
             yield Request(url, headers=self.headers, cookies=False)
 
-    def parse(self, response):
-        for price in response.css('.product-card__price'):
-            yield {'price': price.css('::text').get()}
-
+    def parse(self, response):        
+        product_card = '.product-card'
+        
+        for product in response.css('.product-card__detail'):       
+            yield {
+                'brand': 'Carrefour',
+                'price': self.get_only_text(product.css('.product-card__prices-container ::text').get()),
+                'name': self.get_only_text(product.css('.product-card__title ::text').get()),
+                'url': self.site_base_url + product.css('.product-card__title > .product-card__title-link ::attr(href)').get(),
+            }
+            
         for next_page in response.css('.pagination__row > a'):
             yield response.follow(next_page, self.parse, headers=self.headers, cookies=False)
+
+    def get_only_text(self, text):
+        """
+        Replace unwanted characters with whitespace
+        """
+        
+        unwanted_chars = ['\n', '\t']
+        unwanted_chars_strip = [' ']
+        
+        just_text = text
+        
+        for c in unwanted_chars:
+            just_text = just_text.replace(c, '')
+
+        for c in unwanted_chars_strip:
+            just_text = just_text.strip(c)
+
+        return just_text
